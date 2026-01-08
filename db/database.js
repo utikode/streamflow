48bada215f4c1dec8e2a8672d80d8d9cb3a5633a
utikode <utilasinah@gmail.com>
2026-01-09 06:20:02 +0700

    Replace bcrypt with bcryptjs in database.js

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const dbDir = path.join(__dirname);
 if (!fs.existsSync(dbDir)) {
   fs.mkdirSync(dbDir, { recursive: true });
