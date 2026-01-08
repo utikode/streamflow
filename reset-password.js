da95601d809701d4ed932e7c2a344070f5cd22d8
utikode <utilasinah@gmail.com>
2026-01-09 06:19:00 +0700

    Switch bcrypt to bcryptjs for password hashing

--- reset-password.js
+++ reset-password.js
@@ -1,5 +1,5 @@
 const readline = require('readline');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { db } = require('./db/database');
 const User = require('./models/User');
 require('dotenv').config();
